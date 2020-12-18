1874d433b93973203dd499677320abfc9b4fc62d
kaustavr19 <kaustavr19@users.noreply.github.com>
2020-12-18 20:24:16 +0530

    Initial commit datastructures.cpp input_output_rahul.cpp

---
+++ input_output_rahul.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int amount1, amount2, total_amount;
+    cin>>amount1;
+    cin>>amount2;
+    total_amount = amount1+amount2;
+    cout<<"The total amount is: "<<total_amount<<endl;
+    return 0;
+}
+//
+// Created by RIJU25 on 18-12-2020.
+//
+
