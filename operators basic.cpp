57821a58137ba3eaa301ff5069d34ee1f1d49f01
Kaustav Roy <54073521+kaustavr19@users.noreply.github.com>
2020-12-27 21:50:40 +0530

    break,continue,switch case,operators basic

---
+++ operators basic.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int x = 2;
+(x & 1) ? printf("true") : printf("false");
+printf("%d",3 * 2--);
+return 0;
+}
---
+++ operators basic2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i = 10;
+i++;
+i * i;
+printf("%d\n",i);
+return 0;
+}
